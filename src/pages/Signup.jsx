915aeb15546ce2a54888844c081c6c1c09a01224
Harsh-sjce <harsh.sjce@gmail.com>
2024-08-25 14:56:29 +0530

    used render link instead of localhost

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -31,7 +31,7 @@ const Signup = () => {
         alert("All fields are required");
       } else {
         const response = await axios.post(
-          "http://localhost:1000/api/v1/sign-up",
+          "https://bookheaven-server-1.onrender.com/api/v1/sign-up",
           Data
         );
         setData({ username: "", email: "", password: "", address: "" });
